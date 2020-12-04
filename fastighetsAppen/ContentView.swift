7335ed4a2d4e538b145b6cb38cf1d94974b935bc
Michele Byman <michelebyman@gmail.com>
2020-12-04 11:49:10 +0100

    Initial Commit

---
+++ fastighetsAppen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fastighetsAppen
+//
+//  Created by Michele Byman on 2020-12-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fastighetsAppen/fastighetsAppenApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastighetsAppenApp.swift
+//  fastighetsAppen
+//
+//  Created by Michele Byman on 2020-12-04.
+//
+
+import SwiftUI
+
+@main
+struct fastighetsAppenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
